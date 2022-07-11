914eebe17d8dd7fe99327be5eeae88fafda1a519
jdog <thuz3rfr13ndly@gmail.com>
2022-07-11 00:00:14 -0600

    more tests make jest dependency

--- tests/squadMember.test.js
+++ tests/squadMember.test.js
@@ -1,7 +1,14 @@
 const squadMember = require('.././lib/squadMember')
 
-
 describe('squadMember', () => {
-     expect ();
+    describe("squadMember constructor", () => {
+        it("It should have a name, id, email, attribute1", () => {
+            const eachSquad = new squadMember("string", 1, "name@me.com", "attribute");
+            expect(eachSquad.name).toEqual("string");
+            expect(eachSquad.id).toEqual(1);
+            expect(eachSquad.email).toEqual("name@me.com");
+            expect(eachSquad.attribute1).toEqual("attribute");
+        });
+    });
   });
   
